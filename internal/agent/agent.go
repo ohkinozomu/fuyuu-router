a859369dabbb0ee01e662f6d11c9e154050f8d69
Ohki Nozomu <nozomunoise@gmail.com>
2023-11-14 18:17:08 +0900

    Move payload processing to the server (#42)

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -1,20 +1,36 @@
 package agent
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/eclipse/paho.golang/paho"
+	"github.com/klauspost/compress/zstd"
 	"github.com/ohkinozomu/fuyuu-router/internal/common"
 	"github.com/ohkinozomu/fuyuu-router/pkg/data"
 	"github.com/ohkinozomu/fuyuu-router/pkg/topics"
+	"github.com/thanos-io/objstore"
+	objstoreclient "github.com/thanos-io/objstore/client"
+	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
 )
 
 type server struct {
-	client      *paho.Client
-	messageChan chan string
+	client       *paho.Client
+	id           string
+	proxyHost    string
+	logger       *zap.Logger
+	protocol     string
+	encoder      *zstd.Encoder
+	decoder      *zstd.Decoder
+	commonConfig common.CommonConfigV2
+	bucket       objstore.Bucket
+	payloadCh    chan []byte
 }
 
 func newServer(c AgentConfig) server {
@@ -24,17 +40,79 @@ func newServer(c AgentConfig) server {
 	if err != nil {
 		c.Logger.Fatal("Error: " + err.Error())
 	}
-	messageChan := make(chan string)
+	payloadCh := make(chan []byte)
 	clientConfig := paho.ClientConfig{
 		Conn:   conn,
-		Router: NewRouter(messageChan, c),
+		Router: NewRouter(payloadCh, c),
 	}
 	client := paho.NewClient(clientConfig)
 
-	return server{
-		client:      client,
-		messageChan: messageChan,
+	var encoder *zstd.Encoder
+	var decoder *zstd.Decoder
+	if c.CommonConfigV2.Networking.Compress == "zstd" {
+		encoder, err = zstd.NewWriter(nil)
+		if err != nil {
+			c.Logger.Fatal(err.Error())
+		}
+
+		decoder, err = zstd.NewReader(nil)
+		if err != nil {
+			c.Logger.Fatal(err.Error())
+		}
 	}
+
+	var bucket objstore.Bucket
+	if c.CommonConfigV2.Networking.LargeDataPolicy == "storage_relay" {
+		objstoreConf, err := os.ReadFile(c.CommonConfigV2.StorageRelay.ObjstoreFile)
+		if err != nil {
+			c.Logger.Fatal(err.Error())
+		}
+		bucket, err = objstoreclient.NewBucket(common.NewZapToGoKitAdapter(c.Logger), objstoreConf, "fuyuu-router-hub")
+		if err != nil {
+			c.Logger.Fatal(err.Error())
+		}
+	}
+
+	return server{
+		client:       client,
+		payloadCh:    payloadCh,
+		logger:       c.Logger,
+		commonConfig: c.CommonConfigV2,
+		id:           c.ID,
+		proxyHost:    c.ProxyHost,
+		protocol:     c.Protocol,
+		encoder:      encoder,
+		decoder:      decoder,
+		bucket:       bucket,
+	}
+}
+
+func sendHTTP1Request(proxyHost string, data *data.HTTPRequestData) (string, int, http.Header, error) {
+	var responseHeader http.Header
+
+	url := "http://" + proxyHost + data.Path
+	body := bytes.NewBufferString(data.Body.Body)
+	req, err := http.NewRequest(data.Method, url, body)
+	if err != nil {
+		return "", http.StatusInternalServerError, responseHeader, err
+	}
+	for key, values := range data.Headers.GetHeaders() {
+		for _, value := range values.GetValues() {
+			req.Header.Add(key, value)
+		}
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", http.StatusInternalServerError, responseHeader, err
+	}
+	defer resp.Body.Close()
+	responseBody := new(bytes.Buffer)
+	_, err = responseBody.ReadFrom(resp.Body)
+	if err != nil {
+		return "", resp.StatusCode, responseHeader, err
+	}
+	return responseBody.String(), resp.StatusCode, resp.Header, nil
 }
 
 func Start(c AgentConfig) {
@@ -120,5 +198,102 @@ func Start(c AgentConfig) {
 	if err != nil {
 		c.Logger.Fatal(err.Error())
 	}
-	select {}
+	for {
+		select {
+		case payload := <-s.payloadCh:
+			go func() {
+				requestPacket, err := data.DeserializeRequestPacket(payload, s.commonConfig.Networking.Format)
+				if err != nil {
+					s.logger.Error("Error deserializing request packet", zap.Error(err))
+					return
+				}
+
+				var responsePacket data.HTTPResponsePacket
+				httpRequestData, err := data.DeserializeHTTPRequestData(requestPacket.HttpRequestData, requestPacket.Compress, s.commonConfig.Networking.Format, s.decoder, s.bucket)
+				if err != nil {
+					s.logger.Error("Error deserializing request data", zap.Error(err))
+					return
+				}
+
+				if s.protocol == "http1" {
+					var responseData data.HTTPResponseData
+					var objectName string
+					httpResponse, statusCode, responseHeader, err := sendHTTP1Request(s.proxyHost, httpRequestData)
+					if err != nil {
+						s.logger.Error("Error sending HTTP request", zap.Error(err))
+						protoHeaders := data.HTTPHeaderToProtoHeaders(responseHeader)
+						// For now, not apply the storage relay to the error
+						responseData = data.HTTPResponseData{
+							Body: &data.HTTPBody{
+								Body: err.Error(),
+								Type: "data",
+							},
+							StatusCode: http.StatusInternalServerError,
+							Headers:    &protoHeaders,
+						}
+					} else {
+						if s.commonConfig.Networking.LargeDataPolicy == "storage_relay" && len(httpResponse) > s.commonConfig.StorageRelay.ThresholdBytes {
+							objectName = s.id + "/" + requestPacket.RequestId + "/response"
+							err := s.bucket.Upload(context.Background(), objectName, strings.NewReader(httpResponse))
+							if err != nil {
+								s.logger.Error("Error uploading object to object storage", zap.Error(err))
+								return
+							}
+						}
+						protoHeaders := data.HTTPHeaderToProtoHeaders(responseHeader)
+
+						var body data.HTTPBody
+						if s.commonConfig.Networking.LargeDataPolicy == "storage_relay" && len(httpResponse) > s.commonConfig.StorageRelay.ThresholdBytes {
+							body = data.HTTPBody{
+								Body: objectName,
+								Type: "storage_relay",
+							}
+						} else {
+							body = data.HTTPBody{
+								Body: httpResponse,
+								Type: "data",
+							}
+						}
+
+						responseData = data.HTTPResponseData{
+							Body:       &body,
+							StatusCode: int32(statusCode),
+							Headers:    &protoHeaders,
+						}
+					}
+					b, err := data.SerializeHTTPResponseData(&responseData, s.commonConfig.Networking.Format, s.encoder)
+					if err != nil {
+						s.logger.Error("Error serializing response data", zap.Error(err))
+						return
+					}
+					responsePacket = data.HTTPResponsePacket{
+						RequestId:        requestPacket.RequestId,
+						HttpResponseData: b,
+						Compress:         s.commonConfig.Networking.Compress,
+					}
+				} else {
+					s.logger.Error("Unknown protocol: " + s.protocol)
+					return
+				}
+
+				responseTopic := topics.ResponseTopic(s.id, requestPacket.RequestId)
+
+				responsePayload, err := data.SerializeResponsePacket(&responsePacket, s.commonConfig.Networking.Format)
+				if err != nil {
+					s.logger.Error("Error serializing response packet", zap.Error(err))
+					return
+				}
+
+				_, err = s.client.Publish(context.Background(), &paho.Publish{
+					Topic:   responseTopic,
+					QoS:     0,
+					Payload: responsePayload,
+				})
+				if err != nil {
+					s.logger.Error("Error publishing response", zap.Error(err))
+					return
+				}
+			}()
+		}
+	}
 }

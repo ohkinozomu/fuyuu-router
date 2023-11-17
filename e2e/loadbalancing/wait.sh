28184102408c19d54f7471c2c44d044d8aa37100
Ohki Nozomu <nozomunoise@gmail.com>
2023-11-18 00:49:56 +0900

    Use mosquitto (#52)

--- e2e/loadbalancing/wait.sh
+++ e2e/loadbalancing/wait.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-while [[ $(kubectl get pods -n nanomq -l app=nanomq -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for nanomq" && sleep 5; done
+while [[ $(kubectl get pods -n mosquitto -l app=mosquitto -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for mosquitto" && sleep 5; done
 while [[ $(kubectl get pods -n fuyuu-router -l app=fuyuu-router-hub -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for hub pod" && sleep 5; done
 while [[ $(kubectl get pods -n fuyuu-router -l app=fuyuu-router-agent-1 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for agent pod" && sleep 5; done
 while [[ $(kubectl get pods -n fuyuu-router -l app=fuyuu-router-agent-2 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for agent pod" && sleep 5; done

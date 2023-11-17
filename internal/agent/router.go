9014fe188586a090a0987e758ff9ac539425769b
Ohki Nozomu <nozomunoise@gmail.com>
2023-11-17 23:23:00 +0900

    Remove (#51)

--- internal/agent/router.go
+++ internal/agent/router.go
@@ -1,12 +1,8 @@
 package agent
 
 import (
-	"context"
-	"time"
-
 	"github.com/eclipse/paho.golang/packets"
 	"github.com/eclipse/paho.golang/paho"
-	"github.com/ohkinozomu/fuyuu-router/internal/common"
 	"go.uber.org/zap"
 )
 
@@ -18,22 +14,6 @@ type Router struct {
 var _ paho.Router = (*Router)(nil)
 
 func NewRouter(payloadCh chan []byte, c AgentConfig, logger *zap.Logger) *Router {
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
-	conn, err := common.TCPConnect(ctx, c.CommonConfig)
-	if err != nil {
-		c.Logger.Fatal("Error: " + err.Error())
-	}
-	clientConfig := paho.ClientConfig{
-		Conn: conn,
-	}
-	client := paho.NewClient(clientConfig)
-
-	_, err = client.Connect(context.Background(), common.MQTTConnect(c.CommonConfig))
-	if err != nil {
-		c.Logger.Fatal("Error connecting to MQTT broker: " + err.Error())
-	}
-
 	return &Router{
 		payloadCh: payloadCh,
 		logger:    logger,

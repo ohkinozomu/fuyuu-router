22b15b4b1391dd44b4b06b2e8bfd012700930e05
ohkinozomu <nozomunoise@gmail.com>
2023-11-29 07:31:39 +0900

    WithLowerEncoderMem

--- internal/hub/server.go
+++ internal/hub/server.go
@@ -143,7 +143,7 @@ func newServer(c HubConfig) server {
 	}
 	var encoder *zstd.Encoder
 	if c.CommonConfigV2.Networking.Compress == "zstd" {
-		encoder, err = zstd.NewWriter(nil, zstd.WithEncoderConcurrency(1))
+		encoder, err = zstd.NewWriter(nil, zstd.WithEncoderConcurrency(1), zstd.WithLowerEncoderMem(true))
 		if err != nil {
 			c.Logger.Fatal(err.Error())
 		}
